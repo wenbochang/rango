4d00ab4f9e727be88e2f1d95fc8e79eab86c4bdf
Wenbo <enquea@gmail.com>
2014-03-13 17:05:26 -0700

    add admin for categories and pages

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,14 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hello World <a href='/rango/about'>About</a>")
+    context = RequestContext(request)
+    context_dict = {'boldmessage': "Bold var from context"}
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-  return HttpResponse("About page <a href='/rango'>Index</a>")
+    context = RequestContext(request)
+    context_dict = {'message': "About var"}
+    return render_to_response('rango/about.html', context_dict, context)
+    
--- tangoApp/urls.py
+++ tangoApp/urls.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,6 +14,14 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
 )
+
+#concatenate media file path
+if settings.DEBUG:
+  urlpatterns += patterns(
+      'django.views.static',
+      (r'media/(?P<path>.*)',
+      'serve',
+      {'document_root': settings.MEDIA_ROOT}),)
