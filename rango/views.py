c4c7fa24f0fd826a9a865e5244666892ec3e5810
Wenbo <enquea@gmail.com>
2014-03-18 16:43:40 -0700

    views for category

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,16 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': "Bold var from context"}
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
+
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
@@ -12,3 +18,17 @@ def about(request):
     context_dict = {'message': "About var"}
     return render_to_response('rango/about.html', context_dict, context)
     
+def category(request, category_name_url):
+    context = RequestContext(request)
+    category_name = category_name_url.replace('_', ' ')
+    context_dict = {'category_name': category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+        context_dict['category'] = category
+        context_dict['pages'] = pages
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
